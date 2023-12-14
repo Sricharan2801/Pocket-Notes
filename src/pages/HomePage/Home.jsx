f197c46904040cedf528a7bf0a28219fe68ac03b
Sri charan <charanbunny3925@gmail.com>
2023-12-14 15:26:25 +0530

    added responsiveness

--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -1,29 +1,43 @@
-import React, { useState } from 'react'
-import styles from "./Home.module.scss"
-import LeftSection from '../../components/HomeComponents/LeftSection'
-import RightSection from '../../components/HomeComponents/RightSection'
-import NotesWindow from '../../components/NotesWindow/NotesWindow'
-import { Routes,Route } from 'react-router-dom'
-import { useNavigate } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import styles from './Home.module.scss';
+import LeftSection from '../../components/HomeComponents/LeftSection';
+import RightSection from '../../components/HomeComponents/RightSection';
+import NotesWindow from '../../components/NotesWindow/NotesWindow';
 
 const Home = () => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 600);
 
-  const navigate = useNavigate()
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 600);
+    };
 
-  window.onload = ()=>{
-    navigate("/welcome")
-  }
- 
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
-    <div className={styles.main} >
-      <LeftSection/>
-     
-      <Routes>
-        <Route path='/welcome' element={ <RightSection />}></Route>
-        <Route path='/notes' element={<NotesWindow/>}/>
-      </Routes>
+    <div className={styles.main}>
+      {isMobile ? (
+        <Routes>
+          <Route path='/' element={<LeftSection />} />
+          <Route path='/notes' element={<NotesWindow />} />
+        </Routes>
+      ) : (
+        <>
+          <LeftSection />
+          <Routes>
+            <Route path='/' element={<RightSection />} />
+            <Route path='/notes' element={<NotesWindow />} />
+          </Routes>
+        </>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
