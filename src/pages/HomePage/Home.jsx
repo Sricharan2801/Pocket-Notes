d6f7218f8cf1ea5e525c1e3eb276cd648d0e6a9b
Sri charan <charanbunny3925@gmail.com>
2023-12-14 13:55:08 +0530

    updated home page

--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -4,15 +4,22 @@ import LeftSection from '../../components/HomeComponents/LeftSection'
 import RightSection from '../../components/HomeComponents/RightSection'
 import NotesWindow from '../../components/NotesWindow/NotesWindow'
 import { Routes,Route } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
+
+  const navigate = useNavigate()
+
+  window.onload = ()=>{
+    navigate("/welcome")
+  }
  
   return (
     <div className={styles.main} >
       <LeftSection/>
      
       <Routes>
-        <Route path='/' element={ <RightSection />}></Route>
+        <Route path='/welcome' element={ <RightSection />}></Route>
         <Route path='/notes' element={<NotesWindow/>}/>
       </Routes>
     </div>
